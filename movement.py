8665839b60d50804281dd19e00906cac3d05aa18
ruojiazhang22 <dzhang2@haverford.edu>
2025-05-02 15:12:49 -0400

    updated movement.py

--- movement.py
+++ movement.py
@@ -8,8 +8,6 @@ tentative_map = load_map("map1.txt")
 x = 0
 y = 0
 
-
-
 # returns player's current location as an (x,y) tuple
 def getCurrentLocation() -> tuple:
     column_number = 0
@@ -28,55 +26,66 @@ def getCurrentLocation() -> tuple:
 # Takes player to new location. Returns True if successful, false otherwise
 def setLocation(x: int, y: int) -> bool:
     current_map = tentative_map
+    previous_x, previous_y = getCurrentLocation()
     if 0 <= x < len(current_map) and 0 <= y < len(current_map[0]):
-        if current_map[x][y] == 1 or current_map[x][y] == 3:
-            print(f"Teleportation successful, you are now at ({x+1}, {y+1})")
-            return True, current_map[x][y] == 3
+        if current_map[x-1][y-1] == 1 or current_map[x-1][y-1] == 3:
+            print(f"Teleportation successful, you are now at ({x}, {y})")
+            current_map[previous_x-1][previous_y-1] = 1
+            current_map[x-1][y-1] = 3
+            print(current_map)
+            return True
         else:
             print("Location unavailable.")
             return False
     else:
         print("Invalid coordinates.")
         return False
-   
+    
 # print goal reached once player had arrived at the final destination
-def goalReached() -> tuple[bool, str]:
+def goalReached() -> bool:
     current_map = tentative_map
     for row in tentative_map:
         if 2 in row:
-            return False, "goal not reached yet."
-    return True, "you have reached the goal!"
+            print("Ouch, You have not found Mr.KBLALA yet")
+            return False
+    print("you have found Mr.KBLALA! Get ready to fight!")
+    return True
 
-def canGoNorth(x: int, y: int) -> bool:
+def canGoNorth() -> bool:
+    x, y = getCurrentLocation()
     current_map = tentative_map
     if 1 <= x < len(current_map) and 0 <= y < len(current_map[0]):
         if current_map[x - 1][y] == 1 or current_map[x - 1][y] == 2:
             return True  # able to go north
     return False  # either out of bounds or blocked (i.e.: 0)
     
-def canGoSouth(x: int, y: int) -> bool:
+def canGoSouth() -> bool:
+    x, y = getCurrentLocation()
     current_map = tentative_map
     if 0 <= x < len(current_map)-1 and 0 <= y < len(current_map[0]):
         if current_map[x + 1][y] == 1 or current_map[x + 1][y] == 2:
             return True  # able to go south
     return False  # either out of bounds or blocked (i.e.: 0)
 
-def canGoEast(x: int, y: int) -> bool:
+def canGoEast() -> bool:
+    x, y = getCurrentLocation()
     current_map = tentative_map
     if 0 <= x < len(current_map) and 0 <= y+1 < len(current_map[0]):
         if current_map[x][y+1] == 1 or current_map[x][y+1] == 2:
             return True  # able to go east
     return False  # either out of bounds or blocked (i.e.: 0)
 
-def canGoWest(x: int, y: int) -> bool:
+def canGoWest() -> bool:
     current_map = tentative_map
+    x, y = getCurrentLocation()
     if 0 <= x < len(current_map) and 1 <= y < len(current_map[0]):
         if current_map[x][y-1] == 1 or current_map[x][y-1] == 2:
             return True  # able to go west
     return False  # either out of bounds or blocked (i.e.: 0)
 
 def goNorth():
-    global x, y, tentative_map, previous_positions
+    global tentative_map, previous_positions
+    x, y = getCurrentLocation()
     if x > 0 and tentative_map[x - 1][y] in [1, 2]:
         tentative_map[x][y] = 1  # Set old position to 1
         previous_positions.append((x, y))  # add old position to previous_positions list
@@ -84,7 +93,8 @@ def goNorth():
         tentative_map[x][y] = 3  # Set new position
 
 def goSouth():
-    global x, y, tentative_map, previous_positions
+    global tentative_map, previous_positions
+    x, y = getCurrentLocation()
     if x < len(tentative_map) - 1 and tentative_map[x + 1][y] in [1, 2]:
         tentative_map[x][y] = 1  # Set old position to 1
         previous_positions.append((x, y))  # add old position to previous_positions list
@@ -92,7 +102,8 @@ def goSouth():
         tentative_map[x][y] = 3  # Set new position
 
 def goEast():
-    global x, y, tentative_map, previous_positions
+    global tentative_map, previous_positions
+    x, y = getCurrentLocation()
     if y < len(tentative_map[0]) - 1 and tentative_map[x][y + 1] in [1, 2]:
         tentative_map[x][y] = 1  # Set old position to 1
         previous_positions.append((x, y))  # add old position to previous_positions list
@@ -100,7 +111,8 @@ def goEast():
         tentative_map[x][y] = 3  # Set new position
 
 def goWest():
-    global x, y, tentative_map, previous_positions
+    global tentative_map, previous_positions
+    x, y = getCurrentLocation()
     if y > 0 and tentative_map[x][y-1] in [1, 2]:
         tentative_map[x][y] = 1  # Set old position to 1
         previous_positions.append((x, y))  # add old position to previous_positions list
